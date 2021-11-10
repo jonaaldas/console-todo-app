b4bebc652adcb6ab105a56f609bf99332bea2c61
Jonathan <jonaaldas@gmail.com>
2021-11-09 22:49:29 -0500

    working with JS

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello")
+//1 "new" - add a todo
+//2 "list" - list all todods 
+//3 "delete" - remove specifictodos
+//4 "quit" - quit the app
+
+
+// adding a todo type a command
+
+let askQuestion = prompt("Welcome, choose between - new, list, delete and quit");
+let quit = "quit"
+
+while(askQuestion !== quit.toLocaleLowerCase()){
+  // let options = ["new", "list", "delete", "quit"];
+  console.log("its working");
+}
